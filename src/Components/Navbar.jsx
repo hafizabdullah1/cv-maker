d8dd275af425c2d56eb501280f23ffedc1f77ed4
Abdullah2723 <127143084+Abdullah2723@users.noreply.github.com>
2024-05-23 23:42:53 +0500

    some changes in navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function NavBar() {
   return (
     <div className='pt-6'>
-      <nav className='mx-auto flex flex-col lg:flex-row justify-between items-center text-center max-w-[1371px] border border-blue-500 px-4 lg:px-12 py-3'>
+      <nav className='flex flex-col lg:flex-row justify-between items-center text-center max-w-[1371px] border border-blue-500 px-4 lg:px-12 py-3'>
         <img src="../img/image 1.png" alt="" className='mb-4 lg:mb-0' />
         <div className='flex flex-col lg:flex-row items-center'>
           <button className='border-2 py-2 px-4 lg:py-3 lg:px-6 mb-2 lg:mb-0 capitalize rounded-xl hover:bg-[#635EA3] hover:text-white border-[#635EA3] text-[#635EA3]'>
